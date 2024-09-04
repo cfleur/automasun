2e56fbaba44fc6cedddd9f6f322f6eb75e3a0acf
cfleur <cfleur@users.noreply.github.com>
2024-09-04 12:11:05 +0300

    Adds parsing for case log pressure file and refactors

--- modules/ioutils.py
+++ modules/ioutils.py
@@ -5,6 +5,7 @@ import shutil
 
 from pathlib import Path
 from datetime import datetime
+from typing import Union
 
 from . import timeutils
 
@@ -30,11 +31,15 @@ def csv_get_col(f, col_num, sep=','):
     return df[df.columns[col_num]]
 
 
-def preprocess_pressure_file(input_pressure_file: str, 
-                             out_dir_name: str,
-                             out_file_name: str,
-                             pressure_correction: any,
-                             v: bool = False) -> None:
+def preprocess_pressure_file(
+        input_pressure_file: str,
+        out_dir_name: str,
+        out_file_name: str,
+        pressure_correction: Union[None, float, list] = None,
+        in_sep: Union[None, str] = None,
+        v: bool = False
+) -> None:
+    input_file_type = str(input_pressure_file).split(sep='.')[-1]
 
     # create directories needed in output file path
     out_dir = Path(out_dir_name)
@@ -42,28 +47,36 @@ def preprocess_pressure_file(input_pressure_file: str,
 
     out_file = out_dir/out_file_name
 
-    parse_pressure_file(input_pressure_file, out_file, pressure_correction=pressure_correction, v=v)
+    parse_pressure_file(
+        input_pressure_file,
+        input_file_type,
+        out_file,
+        pressure_correction=pressure_correction,
+        in_sep=in_sep,
+        v=v
+    )
 
 
 def parse_pressure_file(
         input_file_path: str, 
-        # input_file_type: str,
+        input_file_type: str,
         output_file_path: str, 
-        pressure_correction: None = None,
-        in_sep: str ='\s\s+',
+        pressure_correction: Union[None, float, list] = None,
+        in_sep: Union[None, str] = None,
         out_sep: str ='\t', 
-        in_col_names: None = None, 
-        out_col_names: None = None,
-        v: bool = False) -> None:
-    """Takes aws .lst file as input and creates a .csv file with data necessary for retrieval algorithm. Pre
+        in_col_names: Union[None, dict] = None,
+        out_col_names: Union[None, dict] = None,
+        v: bool = False,
+        q: bool = False
+) -> None:
+    """Takes aws .lst or txt log pressure file as input and
+    creates a .csv file with data necessary for retrieval algorithm.
     """
-    
-    out_file = Path(output_file_path)
-
+    if v:
+        print('Creating formatted pressure file.')
     # set default values for mutable type arguments 
     if in_col_names == None:
         in_col_names = {}
-    
     if out_col_names == None:
         out_col_names = {
             'date': 'UTCdate', 
@@ -71,43 +84,95 @@ def parse_pressure_file(
             'pressure': 'BaroTHB40'
         }
     
-    # read in pressure file
-    df = pd.read_csv(input_file_path, sep=in_sep, engine='python').drop(0)
+    if input_file_type == 'lst':
+        if in_sep == None:
+            in_sep = '\s\s+'
+        df = pd.read_csv(input_file_path, sep=in_sep, engine='python').drop(0)
 
-    # parse timestamp
-    if 'timestamp_col_name' in in_col_names: 
-        timestamp_col_name = in_col_names['timestamp_col_name']
-    else: 
-        timestamp_col_name = df.columns[0]
+        # parse timestamp
+        if 'timestamp_col_name' in in_col_names:
+            timestamp_col_name = in_col_names['timestamp_col_name']
+        else:
+            timestamp_col_name = df.columns[0]
+        timestamps = list(df[timestamp_col_name])
+        timestamp_df = timeutils.timestamp_to_date_time(timestamps)
 
-    timestamps = list(df[timestamp_col_name])
-    timestamp_df = timeutils.timestamp_to_date_time(timestamps)
-
-    # apply correction to pressure column if correction provided
-    _pressure = df['P_ST']
-
-    if pressure_correction == None:
-        print('No pressure correction applied.')
-    elif type(pressure_correction) == float:
-        _pressure += pressure_correction # subtract the pressure_correction from each measurement if offest is a scalar
-        print(f'Scalar pressure offest of {pressure_correction:.5f} applied.')
-    elif len(pressure_correction) == len(_pressure):
-        _pressure += pressure_correction # subtract the pressure_correction vector from the pressure measurement vector if pressure_correction is a vector
-        print('Vector pressure correction applied.')
+        _pressure = df['P_ST']
+        _out_pressure = pd.DataFrame(
+            np.array([
+                timestamp_df['date'],
+                timestamp_df['time'],
+                apply_pressure_correction(_pressure, pressure_correction, q)
+            ]).T,
+            columns=[
+                out_col_names['date'],
+                out_col_names['time'],
+                out_col_names['pressure']
+            ])
+    elif input_file_type == 'txt':
+        if in_sep == None:
+            in_sep = '\s+'
+        df = pd.read_csv(input_file_path, sep=in_sep, engine='python', skiprows=2, header=None)
+        _pressure = df[9]
+        _out_pressure = pd.DataFrame(
+            np.array([
+                df[0],
+                df[1],
+                apply_pressure_correction(_pressure, pressure_correction, q),
+                df[12],
+                df[15]
+            ]).T,
+            columns=[
+                out_col_names['date'],
+                out_col_names['time'],
+                out_col_names['pressure'],
+                'TemperatureC',
+                'RelativeHumidity'
+            ])
     else:
-        raise ValueError('Pressure correction must be either None (default), a float, or a numpy array of floats of same length as number of pressure measurements.')
+        raise ValueError(
+            f'Supported input file types: .lst, .txt.'
+            f' Got {input_file_type}.'
+        )
 
-    # create new dataframe
-    _out_pressure = pd.DataFrame(np.array([timestamp_df['date'], timestamp_df['time'], _pressure]).T,
-                                 columns=[out_col_names['date'], out_col_names['time'], out_col_names['pressure']])
-    
     # export
+    out_file = Path(output_file_path)
     _out_pressure.to_csv(out_file, index=False, sep=out_sep)
 
-    print(f'{out_file.name} pressure file written {datetime.now().time()}.')
-
+    if not q:
+        print(f'{out_file.name} pressure file written {datetime.now().time()}.')
     if v:
         print(f'Pressure file location: {out_file}')
+
+
+def apply_pressure_correction(
+        pressure_vector: pd.Series,
+        pressure_correction: Union[None, float, list] = None,
+        q: bool = False
+) -> pd.Series:
+    if pressure_correction == None:
+        if not q:
+            print('No pressure correction applied.')
+    elif type(pressure_correction) == float:
+        # subtract the pressure_correction
+        # from each measurement if offest is a scalar
+        pressure_vector += pressure_correction
+        if not q:
+            print(f'Scalar pressure offest of {pressure_correction:.5f} applied.')
+    else:
+        try:
+            len(pressure_correction) == len(pressure_vector)
+            # subtract the pressure_correction vector from the
+            # pressure measurement vector if pressure_correction is a vector
+            pressure_vector += pressure_correction
+            if not q:
+                print('Vector pressure correction applied.')
+        except:
+            raise ValueError(
+                'Pressure correction must be either None (default), a float,'
+                ' or an array of floats of same length as number of pressure measurements.'
+            )
+    return pressure_vector
 
 
 def filter_move_files(
