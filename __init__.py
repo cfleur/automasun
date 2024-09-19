6dc2fafbb7a0ebe16b8f0a055dca8032e220ef3a
cfleur <cfleur@users.noreply.github.com>
2024-09-19 14:41:22 +0300

    Adds functions to find unparsed pressure files
    Functions to read files in a folder,
    parse dates from file names,
    create file names from dates,
    compare set difference,
    read yaml config files

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from .modules import (
-    analyze,
     ioutils,
     timeutils
 )
--- modules/ioutils.py
+++ modules/ioutils.py
@@ -1,11 +1,12 @@
 import h5py
+import shutil
+import yaml
 import pandas as pd
 import numpy as np
-import shutil
 
 from pathlib import Path, PosixPath
-from datetime import datetime
-from typing import Union
+from datetime import datetime, date as Date
+from typing import Union, List
 
 from . import timeutils
 
@@ -40,6 +41,20 @@ def create_file_path(
     return dir/file_name
 
 
+def get_file_extension(
+        file_path_or_name: Union[str, PosixPath],
+        v: bool = True
+) -> str:
+    return str(file_path_or_name).split(sep='.')[-1]
+
+
+def read_yaml_config(
+        config_file_path: Union[str, PosixPath]
+) -> dict:
+    with open(config_file_path, 'r') as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
 def parse_pressure_file(
         input_file_path: Union[str, PosixPath],
         output_file_path: Union[str, PosixPath],
@@ -65,6 +80,7 @@ def parse_pressure_file(
             'time': 'UTCtime', 
             'pressure': 'BaroTHB40'
         }
+    #TODO: use function get_file_extension
     input_file_type = str(input_file_path).split(sep='.')[-1]
     if input_file_type == 'lst':
         if in_sep == None:
@@ -201,4 +217,197 @@ def separate_mod_vmr_map(
         glob_pattern = f'*{ext}'
         filter_move_files(src_path, glob_pattern, dest)       
 
-    
+
+def read_file_names(
+        folder_path: Union[str, PosixPath],
+        v: bool = False
+) -> List[str]:
+    """
+    Returns a list of names of files in a folder.
+    """
+    if v:
+        print(f'Reading file names from {folder_path}')
+    file_names = [
+        file.name for file in Path(folder_path).glob('*')
+        if not file.is_dir()
+    ]
+    if v:
+        print(file_names)
+    return file_names
+
+
+def extract_date_from_fname(
+        file_name: str
+) -> Date:
+    """
+    Parses a date from a filename.
+
+    Note: custom parsing instead of datetime's strptime for more
+    flexibility in file names.
+    """
+    file_type = get_file_extension(file_name)
+    if file_type == 'lst':
+        # aws_yyyymmdd.lst
+        date_string = file_name.split('.')[0].split('_')[1]
+        year = date_string[0:4]
+        month = date_string[4:6]
+        day = date_string[6:8]
+    elif file_type == 'txt':
+        # yymmdd_PTU300_log.txt or yymmdd_PTU300_error_log.txt
+        date_string = file_name.split('.')[0].split('_')[0]
+        year = '20' + date_string[0:2]
+        month = date_string[2:4]
+        day = date_string[4:6]
+    elif file_type == 'csv':
+        # datalogger-<location>-yyyymmdd.csv
+        date_string = file_name.split('.')[0].split('-')[2]
+        year = date_string[0:4]
+        month = date_string[4:6]
+        day = date_string[6:8]
+    else:
+        raise TypeError(
+            f'Pressure file type \'{file_type}\' not supported.'
+            ' Supported types: .lst, .txt, .csv'
+        )
+    date = datetime(int(year), int(month), int(day)).date()
+    return date
+
+
+def generate_fname_from_date(
+        date: Date,
+        file_type: str,
+        location: Union[str, None] = None,
+        v: bool = False
+) -> str:
+    """
+    Generates a filename from a date
+    """
+    if file_type == 'lst':
+        # aws_yyyymmdd.lst
+        date_string = date.strftime("%Y%m%d")
+        file_name = f'aws_{date_string}.lst'
+    elif file_type == 'txt':
+        # yymmdd_PTU300_log.txt
+        date_string = date.strftime("%y%m%d")
+        file_name = f'{date_string}_PTU300_log.txt'
+    elif file_type == 'csv':
+        # datalogger-<location>-yyyymmdd.csv
+        date_string = date.strftime("%Y%m%d")
+        if location is not None:
+            file_name = f'datalogger-{location}-{date_string}.csv'
+        else:
+            raise ValueError(
+                'Sensor location value needed for generating csv file name.'
+            )
+    else:
+        raise TypeError(
+            f'Pressure file type \'{file_type}\' not supported.'
+            ' Supported types: .lst, .txt, .csv'
+        )
+    if v:
+        print(
+            f'file name from date: {file_name}'
+        )
+    return file_name
+
+
+def generate_file_list(
+        date_list: Union[list, set],
+        file_type: str,
+        location: Union[str, None] = None,
+        v: bool = False,
+) -> List[str]:
+    """
+    Generates a list of file names from a list of dates
+    for a given file type.
+    """
+    if isinstance(date_list, set):
+        date_list = list(date_list)
+    elif isinstance(date_list, list):
+        pass
+    else:
+        raise TypeError(
+            f'date_list was {type(date_list)} but must be list or set type.'
+        )
+    file_list = []
+    for d in date_list:
+        file_list.append(
+            generate_fname_from_date(
+                d, file_type, location=location, v=v
+            )
+        )
+    return file_list
+
+
+def generate_date_list(
+        folder_path: Union[str, PosixPath],
+        v: bool = False,
+        vv: bool = False
+) -> List[Date]:
+    """
+    Generates a list of date objects from a folder containing
+    file names that include the date.
+    """
+    file_names = read_file_names(folder_path, v=v)
+    date_list = []
+    for f in file_names:
+        try:
+            d = extract_date_from_fname(f)
+            date_list.append(d)
+            if vv:
+                print(f'file\'{f}\': {d} date extracted.')
+        except Exception as e:
+            if v:
+                print(f'* file\'{f}\': {e}')
+    return date_list
+
+
+def generate_set_difference(
+        s1: set,
+        s2: set
+) -> set:
+    """
+    Returns the elements that are in s1 and not in s2.
+    s1 might be a raw data folder and s2 might be a processed data folder.
+    """
+    return s1.difference(s2)
+
+
+def generate_unparsed_pressure_file_list(
+        instrument: str,
+        location: str,
+        config_file: Union[str, PosixPath],
+        v: bool = False,
+        vv: bool = False
+) -> List[Union[str, PosixPath]]:
+    """
+    Takes raw and parsed pressure folders from a config file and
+    compares the contents based on dates in the file names.
+    Returns a list of unparsed pressure files.
+    """
+    config = read_yaml_config(config_file)
+    raw_pressure_folder = config[instrument][location]['raw_pressure_folder']
+    parsed_pressure_folder = config[instrument][location]['parsed_pressure_folder']
+    raw_pressure_dates = generate_date_list(
+        raw_pressure_folder,
+        v=v,
+        vv=vv
+    )
+    parsed_pressure_dates = generate_date_list(
+        parsed_pressure_folder,
+        v=v,
+        vv=vv
+    )
+    unparsed_pressure_dates = generate_set_difference(
+        set(raw_pressure_dates),
+        set(parsed_pressure_dates)
+    )
+    unparsed_pressure_files = generate_file_list(
+        unparsed_pressure_dates,
+        'txt'
+    )
+    return [
+        Path(raw_pressure_folder)/file
+        for file
+        in unparsed_pressure_files
+    ]
