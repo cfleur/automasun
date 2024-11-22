16da7c566cd3ea26b0e22c42c29acb0d565013ee
cfleur <cfleur@users.noreply.github.com>
2024-11-22 10:43:13 +0200

    patch: Updates logging date printout
    - date is printed before log messaged when functions executed from
      pipeline module

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -1,9 +1,23 @@
+"""
+The functions in this file can be executed directly in the command line.
+The name of the function is the first argument that follows calling the module.
+Arguments that that follow are function arguments.
+E.g. from root folder:
+`python -m modules.FMIpipelineutils.pipeline prepare_pressure alternate_config_file`
+When the file is executed as __main__ and not imported (as in above line),
+the call to `sys.argv` returns the following:
+args[0] = current file
+args[1] = function name
+args[2:] = function args : (*unpacked)
+"""
+
 import datetime as dt
 import os
 import sys
 from pathlib import Path
 
 from . import pressureutils, ioutils
+
 
 def setup_environment() -> Path:
     """
@@ -42,9 +56,6 @@ def prepare_pressure(
 
 if __name__ == "__main__":
     args = sys.argv
+    print(args[0], dt.datetime.now(dt.timezone.utc))
     if len(args) > 1:
         globals()[args[1]](*args[2:])
-    # args[0] = current file
-    # args[1] = function name
-    # args[2:] = function args : (*unpacked)
-    print(args[0], dt.datetime.now(dt.timezone.utc))
