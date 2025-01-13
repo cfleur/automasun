1ed9f8bdb271969df7c4ea01c4c0b95e9eb0c925
cfleur <cfleur@users.noreply.github.com>
2025-01-13 17:39:46 +0200

    Adds test for timeutils.date_in_range

--- tests/test_timeutils.py
+++ tests/test_timeutils.py
@@ -1,9 +1,46 @@
+from contextlib import nullcontext
+import datetime as dt
+
 import numpy as np
 import pandas as pd
+import pytest
 
 from ..modules import timeutils
 
 
+@pytest.mark.only
+@pytest.mark.parametrize(
+        "input_date, start_date, end_date, expectation",
+        [
+            pytest.param(
+                dt.date(2000, 1, 13), dt.date(1990, 1, 13), dt.date(2025, 1, 13), nullcontext(True), id="date-in-range"
+                ),
+            pytest.param(
+                dt.date(1, 1, 1), dt.date(1990, 1, 13), dt.date(2025, 1, 13), nullcontext(False), id="date-too-early"
+                ),
+            pytest.param(
+                dt.date(3000, 1, 13), dt.date(1990, 1, 13), dt.date(2025, 1, 13), nullcontext(False), id="date-too-late"
+                ),
+            pytest.param(
+                dt.date(2000, 1, 13), dt.date(3000, 1, 13), dt.date(2025, 1, 13), pytest.raises(ValueError), id="bad-range"
+                )
+        ]
+)
+def test_date_in_range(
+        input_date,
+        start_date,
+        end_date,
+        expectation
+) -> None:
+    with expectation as e:
+        assert timeutils.date_in_range(
+            input_date,
+            start_date,
+            end_date,
+        ) == e
+
+
+# @pytest.mark.only
 def test_format_datestring() -> None:
     original_date: str = "01.09.2021"
     original_format: str = "%d.%m.%Y"
@@ -16,6 +53,7 @@ def test_format_datestring() -> None:
     ) == desired_output
 
 
+# @pytest.mark.only
 def test_timestamp_to_date_time() -> None:
     timestamps = ["2016-06-02 18:00"]
     sep = " "
